16572fb9b500b3cdc34148e6d04ce64eff49fd13
Wesley <rpwesley20@gmail.com>
2023-07-20 17:19:24 -0300

    Primeira versão

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,110 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowMessage(true);
+    navigator.clipboard.writeText("meuemail@meuemail.com");
+  };
+
+  useEffect(() => {
+    console.log("teste");
+    setTimeout(() => {
+      setShowMessage(false);
+      console.log("teste");
+    }, 5000);
+  }, [showMessage]);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Crie seu site personalizado em minutos e atraia mais clientes para o
+          seu negócio com facilidade e profissionalismo.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      </div>
+      <div>
+        <p className={styles.contato}>Contatos:</p>
+        <div className={styles.opcoes}>
+          <div>
+            <Link
+              href="https://wa.me/16988717770?text=Olá,%20gostaria%20de%20um%20orçamento"
+              target="_blank"
+            >
+              <div className={styles.botaoWhatsapp}>
+                <Image
+                  src="/img/whatsapp.png"
+                  alt="Next.js Logo"
+                  width={40}
+                  height={40}
+                />
+                <p>Whatsapp</p>
+              </div>
+            </Link>
+          </div>
+          <div className={styles.img}>
+            <div className={styles.botaoCopiar}>
+              <Image
+                src="/img/email.png"
+                alt="Next.js Logo"
+                width={40}
+                height={40}
+              />
+              <p onClick={() => handleButtonClick()}>meuemail@meuemail.com</p>
+            </div>
+            {showMessage ? (
+              <div className={styles.done1}>
+                <Image src="/img/done.png" alt="Done" width={40} height={40} />
+              </div>
+            ) : (
+              <div className={styles.done2}></div>
+            )}
+          </div>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <Image src='/img/MGTech.png' alt='MGTech' width={300} height={300} />
       </div>
 
       <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
+        <p className={styles.card}>
+          <h2>Tecnologia</h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            Focados em trazer sempre melhores e mais atuais soluções
+            tecnológicas disponíveis no mercado.
           </p>
-        </a>
+        </p>
+
+        <p className={styles.card}>
+          <h2>Facilidade</h2>
+          <p>
+            Entre em contato conosco, estamos sempre a disposição para levar a
+            melhor experiência aos nossos clientes.
+          </p>
+        </p>
+
+        <p className={styles.card}>
+          <h2>Solução</h2>
+          <p>
+            Sabemos que cada negócio tem sua demanda por isso procuramos atender
+            nossos cliente de maneira acertiva.
+          </p>
+        </p>
+
+        <p className={styles.card}>
+          <h2>Negócios</h2>
+          <p>
+            Alavanque seus negócios de paginas estaticas para apresentar seu
+            negócio até sistemas complexos para gerir suas empresa
+          </p>
+        </p>
       </div>
     </main>
-  )
+  );
 }
